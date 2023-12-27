0cd1a6fbed364c6f062443160adaa7ba664c5ae4
MrMelon54 <github@mrmelon54.com>
2023-12-27 12:54:28 +0000

    Remove unused login config

--- cmd/mc-upload-api/config.go
+++ cmd/mc-upload-api/config.go
@@ -4,12 +4,6 @@ import "github.com/mrmelon54/mc-upload-api/uploader"
 
 type Config struct {
 	Listen     string                    `yaml:"listen"`
-	Login      LoginConfig               `yaml:"login"`
 	Modrinth   uploader.ModrinthConfig   `yaml:"modrinth"`
 	Curseforge uploader.CurseforgeConfig `yaml:"curseforge"`
-}
-
-type LoginConfig struct {
-	Url   string `yaml:"url"`
-	Owner string `yaml:"owner"`
 }
